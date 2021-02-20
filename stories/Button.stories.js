cdc72d95298fba4828ffd7ea0b8c493b5910c930
dilandurmaz <45289986+dilandurmaz@users.noreply.github.com>
2021-02-20 19:25:10 +0300

    profilebox

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -4,13 +4,24 @@ import Button from '../components/button'
 import NavigationButton from '../components/navigation-button'
 import Navigation from '../components/navigation'
 import {Home} from '../components/icons'
-import TitleBold from '../components/title-bold'
+import TitleBold from '../components/text-title'
+import ThemeButton from '../components/theme-button'
+import Stack from '../components/stack'
 
 export default {
   title: 'Buttons'
 }
 
 export const Normal = () => <Button>Save</Button>
+
+export const Theme = () =>
+    <Stack column >
+    <ThemeButton>Save</ThemeButton>
+    <ThemeButton full>Save Full</ThemeButton>
+    <ThemeButton full big>Save Big Button</ThemeButton>
+</Stack>
+
+
 
 export const NavButton = () => (
   <NavigationButton>
