00aa457b41ff62d5ad97ba2d9265bd72147215de
dilandurmaz <45289986+dilandurmaz@users.noreply.github.com>
2021-02-28 14:38:18 +0300

    tweet

---
+++ pages/api/tweet.js
@@ -0,0 +1,16 @@
+
+import Twit from '../../lib/twit'
+
+export default (req, res) => {
+  Twit.get('search/tweets', { from: 'ademilter', count: 20 }, function (
+    err,
+    data,
+    response
+  ) {
+    if (err) {
+      return res.status(400).json({ message: 'Ups! bir hata oldu.' })
+    }
+
+    res.status(200).json(data)
+  })
+}
